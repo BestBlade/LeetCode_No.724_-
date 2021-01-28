a9f3718a0836a6d23a992ed2c27b9f6d725b92ac
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-28 22:57:20 +0800

    Add files via upload

---
+++ leetcode_no724_寻找数组的中心索引_w1.cpp
@@ -0,0 +1,108 @@
+﻿/* ------------------------------------------------------------------|
+给你一个整数数组 nums，请编写一个能够返回数组 “中心索引” 的方法。
+
+数组 中心索引 是数组的一个索引，其左侧所有元素相加的和等于右侧所有元素相加的和。
+
+如果数组不存在中心索引，返回 -1 。如果数组有多个中心索引，应该返回最靠近左边的那一个。
+
+注意：中心索引可能出现在数组的两端。
+
+ 
+
+示例 1：
+
+输入：nums = [1, 7, 3, 6, 5, 6]
+输出：3
+解释：
+索引 3 (nums[3] = 6) 的左侧数之和 (1 + 7 + 3 = 11)，与右侧数之和 (5 + 6 = 11) 相等。
+同时, 3 也是第一个符合要求的中心索引。
+示例 2：
+
+输入：nums = [1, 2, 3]
+输出：-1
+解释：
+数组中不存在满足此条件的中心索引。
+示例 3：
+
+输入：nums = [2, 1, -1]
+输出：0
+解释：
+索引 0 左侧不存在元素，视作和为 0 ；右侧数之和为 1 + (-1) = 0 ，二者相等。
+示例 4：
+
+输入：nums = [0, 0, 0, 0, 1]
+输出：4
+解释：
+索引 4 左侧数之和为 0 ；右侧不存在元素，视作和为 0 ，二者相等。
+ 
+
+提示：
+
+nums 的长度范围为 [0, 10000]。
+任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-pivot-index
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：16 ms, 在所有 C++ 提交中击败了99.69%的用户
+*	内存消耗：30.3 MB, 在所有 C++ 提交中击败了87.31%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int pivotIndex(vector<int>& nums) {
+    int sum = 0;
+    for (int& num : nums) {
+        sum += num;
+    }
+    int tmp = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (2 * tmp + nums[i] == sum) {
+            return i;
+        }
+        tmp += nums[i];
+    }
+    return -1;
+}
